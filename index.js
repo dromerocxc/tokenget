41ebc96d0802de33283d6cb367e3f4a4e34e57ea
David Romero <david.cibermetal@gmail.com>
2023-05-21 15:49:40 -0400

    ajuste 21052023 2

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
-var jwt = require('jsonwebtoken');
-const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
+const express     = require('express');
+const app         = express();
+const path        = require("path");
+const bodyParser  = require("body-parser");
+var cors          = require('cors');
+const PORT        = process.env.PORT
+var jwt           = require('jsonwebtoken');
+require("dotenv").config();
+const courses = [{ id: 1, name: "Algorithms" },{ id: 2, name: "Software Engineering" },{ id: 3, name: "Human Computer" }];
 
+app.get("/", function(req, res) {res.send(courses); });
+app.use(bodyParser.json())
 
-
-async function getToken(peticion, respuesta){
-    // Podemos acceder a la petición HTTP
-    // SE REQUIERE POR PARTE DEL CLIENTE DETERMINAR DESDE DONDE SE LEE EL EXTERNAL_ID
-    let agenteDeUsuario = peticion.header("user-agent");
-    let SECRET = 'uNQEUqxaldxPY8NNWvENnSaKkN25wZw45H8PoFy1yLi45wM6DGLt2SxWbwtl_-Zz37WdUmfPImmTOIKigKVssA'
+app.post("/get-token", function(peticion, respuesta) {
+    /*let SECRET = 'uNQEUqxaldxPY8NNWvENnSaKkN25wZw45H8PoFy1yLi45wM6DGLt2SxWbwtl_-Zz37WdUmfPImmTOIKigKVssA'
     let KEY_ID = 'app_64667c1c10224c827ee7e161'
     let datos =  {"scope": "user", "external_id": peticion.body.external_id}
+    var token = jwt.sign(datos, SECRET, { header:  {kid: KEY_ID}  });*/
+    console.log(peticion.body.external_id)
+    return respuesta.send(peticion.body)
+});
 
-    var token = jwt.sign(datos, SECRET, { header:  {kid: KEY_ID}  });
-    //console.log(peticion.body)
-    //console.log(token)
-    //respuesta.send("La ruta / solicitada con: " + agenteDeUsuario);
-    respuesta.send(token)
-}
+app.use(cors({credentials: true,origin: true}));
+app.listen(process.env.PORT || 3000, function() {
+    console.log(__dirname)
+    console.log('server started at http://localhost on port ' + PORT, '');});
 
 
-
-express()
-  .use(express.static(path.join(__dirname + "/public")))
-  .use(bodyParser.json())
-  .post("/get-token", getToken)
-  .listen(PORT, () => console.log("listening ossn port " + PORT));
 
 
